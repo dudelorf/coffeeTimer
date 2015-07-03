752447c77520b2e5bcd3b74e219996c574d179cc
Eric Kinzel <ericrkinzel@gmail.com>
2015-07-03 18:51:33 -0500

    did lots of code refactoring. Script now validats method name for recipe

---
+++ php_scripts/checkName.php
@@ -0,0 +1,30 @@
+<?php
+	session_start();
+	
+	$tableName = "recipesid".$_SESSION['userId'];
+	
+	@ $db = new mysqli("localhost", "coffeeTimer", "potato", "coffeetimer");
+			
+	if (mysqli_connect_errno())
+	{
+		echo "Could not connect to database. Try something else.";
+		exit;
+	}
+	
+	$theMethod = $_GET['method'];
+
+	$query = "Select * from $tableName where methodName='$theMethod'";
+
+	$result = $db->query($query);
+
+	if($result->num_rows > 0)
+	{
+		//recipe name already exists
+		echo "false";
+	}
+	else
+	{
+		//name is new and valid
+		echo "true";
+	}
+?>
